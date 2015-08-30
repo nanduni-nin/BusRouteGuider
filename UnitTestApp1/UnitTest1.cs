21bab06fb21a477f8ccdb2c215ff0092541c5753
nanduni-nin <nandunibw@gmail.com>
2015-08-30 14:25:31 +0530

    Project along with Unit Tests created

---
+++ UnitTestApp1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace UnitTestApp1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
